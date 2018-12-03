109da47b25cd4e6911c48bc64ad8c04d3818481a
A Nichols <a.k.nichols@live.com>
2018-12-02 21:47:29 -0500

    added initial files

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import * as $ from 'axios';
+
+
+class App extends Component {
+
+  state = {
+    notesList: [],
+    newNote: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({newNote: event.target.value})
+  }
+
+  handleClick = (event) => {
+    event.preventDefault();
+    $.post('/api/note', {content: this.state.newNote})
+    .then((result) => {
+      console.log(result.data);
+    })
+  }
+
+  componentDidMount(){
+    $.get('/api/notes')
+    .then((result) => {
+      this.setState({notesList: result.data})
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <input val={this.state.newNote} onChange={this.handleChange} />
+          <button onClick={this.handleClick}>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
