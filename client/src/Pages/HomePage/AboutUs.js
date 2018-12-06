6ab9529ea454e9723635256add98a2baf1715682
Cynthia <chicagock@gmail.com>
2018-12-06 14:10:43 -0500

    added npm install mdbreact make sure everyone does install

---
+++ client/src/Pages/HomePage/AboutUs.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Row, Col } from "mdbreact";
+
+import DocsLink from './DocsLink'; 
+
+class AboutUs extends React.Component {
+  state = {
+    
+  };
+
+   render() {
+    return (
+      <Container>
+        <DocsLink></DocsLink>
+         
+      </Container>
+    );
+  }
+}
+
+export default AboutUs;
---
+++ client/src/Pages/HomePage/PaginationPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Pagination, PageItem, PageLink, Container } from "mdbreact";
+import DocsLink from "./DocsLink";
+
+const PaginationPage = props => {
+  return (
+    <Container>
+     
+    </Container>
+  );
+};
+
+export default PaginationPage;
