0459f4262de8a907fcf57dd832d55bce0a3a987b
Makiko Vaughan <makikovaughan@Makikos-MacBook-Pro.local>
2018-12-07 12:02:57 -0500

    Updated user collection and created provider page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import Homepage from './Pages/HomePage/homepage';
 import Clientpage from './Pages/ClientPage/clientpage';
+import Providerpage from './Pages/ProviderPage/providerpage';
 import * as $ from 'axios';
 import './App.css';
 
@@ -11,7 +12,7 @@ const App = () => (
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/client' component={Clientpage} />
-       
+        <Route exact path='/provider' component={Providerpage} />
       </Switch>
   </BrowserRouter>
 
