aec4a833659e5d01fe7664b4c7eaf40ef49e431c
Ann <anntuck@anntuck.com>
2018-12-08 16:33:59 -0500

    Added fashion page, components, and image files.  Modified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import Homepage from './Pages/HomePage/homepage';
 import Clientpage from './Pages/ClientPage/clientpage';
+import Fashionpage from './Pages/FashionPage/fashionpage';
 import * as $ from 'axios';
 import './App.css';
 import Canvas from './components/Canvas'
@@ -15,6 +16,7 @@ const App = () => (
         <Route exact path='/' component={Homepage} />
         <Route exact path='/client' component={Clientpage} />
         <Route exact path='/provider' component={Providerpage} />
+        <Route exact path='/fashion' component={Fashionpage} />
         <Route exact path='/canvas' component={Canvas} />
       </Switch>
   </BrowserRouter>
