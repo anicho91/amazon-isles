7d51caf312aa7b87d7dceb9528a23eda4afdc648
Cynthia <chicagock@gmail.com>
2018-12-12 09:58:53 -0500

    updated Slider1 file name to capital

--- client/src/Pages/HomePage/slider1.js
+++ client/src/Pages/HomePage/slider1.js
@@ -9,19 +9,19 @@ class Slider1 extends Component {
     render() {
         return (
             <div>
-            <Jumbotron className='placeholder'>
-                <h2 className='animated fadeInUp delay-5s  z-depth-5'><i> MADE TO MEASURE: BECAUSE YOU ARE UNIQUE AND YOUR CLOTHES SHOULD BE TOO.</i></h2><br></br>
-                <h3><b>  "NOT A LOOSE FIT, NOT A TIGHT FIT, IT'S YOUR FIT" </b></h3>
-            </Jumbotron>
-          
+                <Jumbotron className='placeholder'>
+                    <h2 className='animated fadeInUp delay-5s  z-depth-5'><i> MADE TO MEASURE: BECAUSE YOU ARE UNIQUE AND YOUR CLOTHES SHOULD BE TOO.</i></h2><br></br>
+                    <h3><b>  "NOT A LOOSE FIT, NOT A TIGHT FIT, IT'S YOUR FIT" </b></h3>
+                </Jumbotron>
+
                 <video class="video-background" preload="true" muted="true" autoplay="true" loop="true">
                     <source src="https://videos.files.wordpress.com/XtXvuOGo/1080_30_29-77_dec122018_01_hd.mp4" type="video/mp4" />
                 </video>
 
             </div>
-                );
-                }
-                }
-                   
-                
+        );
+    }
+}
+
+
 export default Slider1;
