299e8fad642df34ae4bc0e45d2cbb21aaae90456
Cynthia <chicagock@gmail.com>
2018-12-12 05:32:13 -0500

    homepage look changes

--- client/src/Pages/HomePage/OtherServices.js
+++ client/src/Pages/HomePage/OtherServices.js
@@ -9,10 +9,10 @@ class OtherServices extends Component {
         <Row>
             <Col className="shadow-box-example z-depth-5">
                 <Card className="card animate fadeInDown one ">
-                    < CardImage className="card-img-top" src="https://princefan046.com/wp-content/uploads/2018/12/sewingnew.jpg" alt="Card image cap"></ CardImage>
+                    < CardImage className="card-img-top" src="https://princefan046.com/wp-content/uploads/2018/12/ezgif.com-resize-1ttt.jpg" alt="Card image cap"></ CardImage>
                     <CardBody>
-                        <h4 className="card-title"><a>Custom Tailors</a></h4>
-                        <p className="card-text">Your New Secret Weapon</p>
+                        <h4 className="card-title"><a>Custom Clothing</a></h4>
+                        <p className="card-text"></p>
                         <a href="#" className="btn btn-secondary">See More</a>
                     </CardBody>
                 </Card>
@@ -21,30 +21,30 @@ class OtherServices extends Component {
                 <Card className="card animate fadeInDown two">
 
 
-                    < CardImage className="card-img-top" src="https://princefan046.com/wp-content/uploads/2018/12/ryannew.jpg" alt="Card image cap"></ CardImage>
+                    < CardImage className="card-img-top" src="https://princefan046.com/wp-content/uploads/2018/12/20181212_040942.png" alt="Card image cap"></ CardImage>
                     <CardBody>
-                        <h4 className="card-title"><a>Childcare </a></h4>
-                        <p className="card-text">Trusted Professionals</p>
+                        <h4 className="card-title"><a>Custom Shoes </a></h4>
+                        <p className="card-text"></p>
                         <a href="#" className="btn btn-secondary">See More</a>
                     </CardBody>
                 </Card>
             </Col>
             <Col className="shadow-box-example z-depth-5">
                 <Card className="card animate fadeInDown three">
-                    < CardImage className="card-img-top" src="https://princefan046.com/wp-content/uploads/2018/12/cleaningnew.jpg" alt="Card image cap"></ CardImage>
+                    < CardImage className="card-img-top" src="https://princefan046.com/wp-content/uploads/2018/12/20181212_040531.png" alt="Card image cap"></ CardImage>
                     <CardBody>
-                        <h4 className="card-title"><a>Cleaning Services</a></h4>
-                        <p className="card-text">Ain't Nobody Got Time 4That!</p>
+                        <h4 className="card-title"><a>Custom Jewelry</a></h4>
+                        <p className="card-text"></p>
                         <a href="#" className="btn btn-secondary">See More</a>
                     </CardBody>
                 </Card>
             </Col>
             <Col className="shadow-box-example z-depth-5">
                 <Card className="card animate fadeInDown four">
-                    < CardImage className="card-img-top" src="https://princefan046.com/wp-content/uploads/2018/12/catsnew.jpg" alt="Card image cap"></ CardImage>
+                    < CardImage className="card-img-top" src="https://princefan046.com/wp-content/uploads/2018/12/20181212_041029.png" alt="Card image cap"></ CardImage>
                     <CardBody>
-                        <h4 className="card-title"><a>Pet Sitters</a></h4>
-                        <p className="card-text">Because They're Family & More</p>
+                        <h4 className="card-title"><a>Custom Knits</a></h4>
+                        <p className="card-text"></p>
                         <a href="#" className="btn btn-secondary">See More</a>
                     </CardBody>
                 </Card>
