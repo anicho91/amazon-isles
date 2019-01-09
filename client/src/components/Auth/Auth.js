345a0fffa33e4463b5fbafadb587e282db3b4120
Anna Nichols <a.k.nichols@live.com>
2019-01-09 11:34:32 -0500

    Update Auth.js

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -105,7 +105,7 @@ export function logout() {
     localStorage.removeItem('name');
     localStorage.removeItem('expires_in');
     localStorage.setItem('expires_at', 0);
-    window.location.replace("https:/amazonisle.auth0.com/v2/logout?returnTo=https%3A%2F%2Famazonisle.herokuapp.com/");
+    window.location.replace("https:/amazonisle.auth0.com/v2/logout?returnTo=https://amazonisle.herokuapp.com/");
 }
 
 export function getAccessToken() {
